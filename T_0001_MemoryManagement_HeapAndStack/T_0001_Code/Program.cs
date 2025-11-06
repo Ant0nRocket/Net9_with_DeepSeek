604940e27c06ad25a8d16374817744b27d4837b8
StefanyakAY <StefanyakAY@rsobks.ru>
2025-11-06 16:11:54 +0500

    T0001_MemoryManagement

---
+++ T_0001_MemoryManagement_HeapAndStack/T_0001_Code/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace T_0001_Code;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number = 10; // 1
+        Person person = new Person(); // 2
+        person.Name = "Anton"; // 3
+    }
+}
+
+class Person
+{
+    public string Name { get; set; }
+}
