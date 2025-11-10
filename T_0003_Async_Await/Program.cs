c4957cc17e62fd507b196f144cd9ef200a09f526
Ant0nRocket <anton.stephanyak@ya.ru>
2025-11-11 00:04:53 +0500

    T_0003_Async_Await

---
+++ T_0003_Async_Await/Program.cs
@@ -0,0 +1,17 @@
+﻿async Task<string> DownloadDataAsync(string url)
+{
+  Console.WriteLine("Начало загрузки. Поток: " + Thread.CurrentThread.ManagedThreadId);
+  using HttpClient client = new HttpClient();
+
+  // Вызываем в другом потоке что-то полезное
+  string data = await client.GetStringAsync(url);
+
+  // Этот код не исполнится, пока не завершится загрузка
+  Console.WriteLine("Загрузка завершена. Поток: " + Thread.CurrentThread.ManagedThreadId);
+
+  // вернётся таска со стрингом в клювике, её нужно будет await'ить
+  return data.ToUpper();
+}
+
+string url = "https://google.com";
+await DownloadDataAsync(url);
