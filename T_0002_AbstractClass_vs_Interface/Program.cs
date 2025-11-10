071327b063f1cd9b3a5561b2adc6a4a632c0989c
StefanyakAY <StefanyakAY@rsobks.ru>
2025-11-10 23:36:10 +0500

    [ref] Old code removed [feat] Theme 0002 added: abstract class and interface

---
+++ T_0002_AbstractClass_vs_Interface/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace T_0002_AbstractClass_vs_Interface;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var printer = new ExactPrinter();
+        printer.Print(new Document());
+    }
+}
+
+class Document : IPrintable
+{
+
+}
+
+interface IPrintable
+{
+
+}
+
+abstract class Printer
+{
+    public abstract void Print(IPrintable document);
+}
+
+class ExactPrinter : Printer
+{
+    public override void Print(IPrintable document)
+    {
+        Console.WriteLine(document.ToString());
+    }
+}
